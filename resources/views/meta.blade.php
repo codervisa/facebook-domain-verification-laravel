b60b1e347bee62769bd2d276827439618dda326a
Bakhti Ali <54646700+codervisa@users.noreply.github.com>
2023-09-26 16:33:38 +0100

    Create meta.blade.php

---
+++ resources/views/meta.blade.php
@@ -0,0 +1,5 @@
+@if ($enabled)
+    <!-- Facebook Verification Domain -->
+    <meta name="facebook-domain-verification" content="{{ $content }}" />
+    <!-- End Facebook Verification Domain -->
+@endif
